a918f1dc684a036d81e1baf4d7ea2094b15b1845
Esteban <estebanescobar232359@correo.itm.edu.co>
2020-07-24 18:50:11 -0500

    vue todo list app finished

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+      <form @submit="addTodo">
+          <input type="text" v-model="title" name="title" placeholder="Add a new todo">
+          <input type="submit" value="SUMBIT" class="btn">
+      </form>
+  </div>
+</template>
+
+<script>
+
+// import { v4 as uuidv4 } from 'uuid';
+
+export default {
+    name: 'AddTodo',
+    data(){
+        return{
+            title: ''
+        }
+    },
+    methods: {
+        addTodo(e){
+            e.preventDefault();
+            const newTodo = {
+                // id: uuidv4(),
+                title: this.title,
+                completed: false
+            }
+            //enviar elobjeto construido con la tarea al componente padre
+            this.$emit('add-todo', newTodo);
+            this.title = '';
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: "5px";
+    }
+
+    input[type="submit"]{
+        flex: 2;
+    }
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <h1>TODOS LIST</h1>
+      <div v-bind:key="todo.id" v-for="todo in todos">
+          <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem';
+export default {
+    name: "TodoList",
+    components: {
+        TodoItem
+    },
+     props: {
+         todos: Array
+     }
+}
+</script>
+
+<style scoped>
+
+</style>
